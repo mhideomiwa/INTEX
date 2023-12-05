873654f0d62eb2effaa32451b911149cb6f61bf4
mhmiwamiwaus <mhmiwa@miwa.us>
2023-12-05 08:26:35 -0700

    changes to get login working.  Still needs some work

--- index.js
+++ index.js
@@ -10,18 +10,19 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "public/pages"));
 
-// const knex = require("knex")({
-//     client: "pg",
-//     connection: {
-//         host: process.env.RDS_HOSTNAME || "",
-//         user: process.env.RDS_USERNAME || "",
-//         password: process.env.RDS_PASSWORD || "",
-//         database: process.env.RDS_DB_NAME || "",
-//         port: process.env.RDS_PORTv|| 5432,
-//         ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
-//     }
-// });
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host: process.env.RDS_HOSTNAME || 'localhost',
+        user: process.env.RDS_USERNAME || 'postgres',
+        password: process.env.RDS_PASSWORD || 'C1$$&!Xi46RRu0HS',
+        database: process.env.RDS_DB_NAME || 'users',
+        port: process.env.RDS_PORTv|| 5432,
+        ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
+    }
+});
 
 //TODO: Add a route to display data and accept data to add to the database
 
@@ -34,7 +35,30 @@ app.get("/signup", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-    res.sendFile(__dirname + "/public/pages/login.html");
+    res.render(__dirname + "/public/pages/login");
 });
 
+app.post("/login", (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    let message = 'Username or password is incorrect.'
+    knex.select().from("users").where("username", username).then(user => {
+        if (user.length > 0) {
+            if (user[0].password === password) {
+                res.render("/public/pages/home.html");
+            } else {
+                // If password is incorrect, display error message in login.ejs
+                res.render( "/public/pages/login", {message});
+            }
+        } else {
+            // If username doesn't exist, display error message in login.ejs
+            res.render("/public/pages/login", message);
+        }
+    });
+});
+
+app.post("/signup", (req, res) => {
+
+
+})
 app.listen(port, () => console.log("Listening on port: " + port + "."));
