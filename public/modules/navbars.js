248d938a3e1755832f2a2dabbb4be6338f2bb4ad
markthuet <119529260+markthuet@users.noreply.github.com>
2023-12-07 19:23:26 -0700

    commenting our code

--- public/modules/navbars.js
+++ public/modules/navbars.js
@@ -1,52 +1,58 @@
-const guestnavbar = `<header>
-        <div>
-            <a href="/"><h1>Provo SMU MH</h1></a>
-        </div>
-        <nav>
-            <a href="/">Home</a>
-            <a href="/dashboard">Dashboard</a>
-            <a href="/survey">Survey</a>
-            <a href="/resources">Resources</a>
-            <div class="login">
-                <a class="login-button" href="login">Login</a>
-            </div>
-        </nav>
-    </header>`
-
-
-const adminnavbar = `
+// Styles for the dropdown menu
+const navbarStyles = `
 <style>
-        /* Dropdown styles */
-        .dropdown {
-            position: relative;
-            display: inline-block;
-        }
+    /* Dropdown styles */
+    .dropdown {
+        position: relative;
+        display: inline-block;
+        z-index: 999; /* Adjust the value as needed */
+    }
 
-        .dropdown-content {
-            display: none;
-            position: absolute;
-            background-color: #f9f9f9;
-            min-width: 120px;
-            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-            z-index: 1;
-        }
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        min-width: 120px;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 1;
+    }
 
-        .dropdown:hover .dropdown-content {
-            display: block;
-        }
+    .dropdown:hover .dropdown-content {
+        display: block;
+    }
 
-        .dropdown-content a {
-            color: black;
-            padding: 12px 16px;
-            text-decoration: none;
-            display: block;
-        }
+    .dropdown-content a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
 
-        .dropdown-content a:hover {
-            background-color: #f1f1f1;
-        }
-    </style>
+    .dropdown-content a:hover {
+        background-color: #f1f1f1;
+    }
+</style>`;
 
+// Navigation bar for guest users
+const guestnavbar = `
+<header>
+    <div>
+        <a href="/"><h1>Provo SMU MH</h1></a>
+    </div>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/dashboard">Dashboard</a>
+        <a href="/survey">Survey</a>
+        <a href="/resources">Resources</a>
+        <div class="login">
+            <a class="login-button" href="login">Login</a>
+        </div>
+    </nav>
+</header>`;
+
+// Navigation bar for admin users
+const adminnavbar = `
+${navbarStyles}
 <header>
     <div>
         <a href="/"><h1>Provo SMU MH</h1></a>
@@ -58,7 +64,6 @@ const adminnavbar = `
         <a href="/resources">Resources</a> 
         <a href="/surveyResults">Survey Results</a>
         <a href="/users">Users</a>
-        <!-- dropdown that -->
         <div class="dropdown">
             <a href="#" class="login-button">My Profile</a>
             <div class="dropdown-content">
@@ -67,41 +72,11 @@ const adminnavbar = `
             </div>
         </div>
     </nav>
-    </header>`
+</header>`;
 
+// Navigation bar for employee users
 const employeenavbar = `
-<style>
-        /* Dropdown styles */
-        .dropdown {
-            position: relative;
-            display: inline-block;
-        }
-
-        .dropdown-content {
-            display: none;
-            position: absolute;
-            background-color: #f9f9f9;
-            min-width: 120px;
-            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-            z-index: 1;
-        }
-
-        .dropdown:hover .dropdown-content {
-            display: block;
-        }
-
-        .dropdown-content a {
-            color: black;
-            padding: 12px 16px;
-            text-decoration: none;
-            display: block;
-        }
-
-        .dropdown-content a:hover {
-            background-color: #f1f1f1;
-        }
-    </style>
-
+${navbarStyles}
 <header>
     <div>
         <a href="/"><h1>Provo SMU MH</h1></a>
@@ -111,7 +86,6 @@ const employeenavbar = `
         <a href="/dashboard">Dashboard</a>
         <a href="/survey">Survey</a>
         <a href="/resources">Resources</a> 
-        <!--            dropdown that -->
         <div class="dropdown">
             <a href="#" class="login-button">My Profile</a>
             <div class="dropdown-content">
@@ -120,41 +94,11 @@ const employeenavbar = `
             </div>
         </div>
     </nav>
-</header>`
+</header>`;
 
+// Navigation bar for user users
 const usernavbar = `
-<style>
-        /* Dropdown styles */
-        .dropdown {
-            position: relative;
-            display: inline-block;
-        }
-
-        .dropdown-content {
-            display: none;
-            position: absolute;
-            background-color: #f9f9f9;
-            min-width: 120px;
-            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-            z-index: 1;
-        }
-
-        .dropdown:hover .dropdown-content {
-            display: block;
-        }
-
-        .dropdown-content a {
-            color: black;
-            padding: 12px 16px;
-            text-decoration: none;
-            display: block;
-        }
-
-        .dropdown-content a:hover {
-            background-color: #f1f1f1;
-        }
-    </style>
-
+${navbarStyles}
 <header>
     <div>
         <a href="/"><h1>Provo SMU MH</h1></a>
@@ -164,7 +108,6 @@ const usernavbar = `
         <a href="/dashboard">Dashboard</a>
         <a href="/survey">Survey</a>
         <a href="/resources">Resources</a> 
-        <!--            dropdown that -->
         <div class="dropdown">
             <a href="#" class="login-button">My Profile</a>
             <div class="dropdown-content">
@@ -173,6 +116,7 @@ const usernavbar = `
             </div>
         </div>
     </nav>
-    </header>`
+</header>`;
 
-module.exports = {guestnavbar, usernavbar, adminnavbar, employeenavbar}
+// Exporting all navigation bars
+module.exports = { guestnavbar, usernavbar, adminnavbar, employeenavbar };
